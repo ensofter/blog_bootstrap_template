5992c2f527fa5616b9369041c8fad25ce0bcee64
Romashov Aleksey <aromashov@ozon.ru>
2023-10-26 20:08:25 +0300

    Rewrite my template for bootrstrap 5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-$('.carousel').carousel({
-    interval: 5000,
+const myCarouselElement = document.querySelector('#carouselExampleIndicators')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 5000,
+  touch: false
 })
